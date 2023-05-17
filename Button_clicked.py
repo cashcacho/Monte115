354e3475496dbc501ac1de51cecd48fe34a612c1
Justine Cacho <justinecacho@gmail.com>
2023-05-17 12:01:34 -0700

    creates button

---
+++ Button_clicked.py
@@ -0,0 +1,18 @@
+#importing tkinter as tk
+import tkinter as tk
+
+#define function and print the words "Button Clicked"
+def button_click ():
+    print("Button clicked")
+
+
+#
+root = tk.Tk()
+root.title("Button Example")
+
+#Create a button that pops out with the words "Click Me!"
+button = tk.Button(root,text="Click Me!", command=button_click)
+button.pack()
+#
+
+root.mainloop()
