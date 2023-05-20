8e86f8d9fcce5203d7994a4602680aed7d7f419d
Justine Cacho <justinecacho@gmail.com>
2023-05-20 16:50:27 -0700

    adds comments

--- Button_clicked.py
+++ Button_clicked.py
@@ -6,13 +6,13 @@ def button_click ():
     print("Button clicked")
 
 
-#
+#Create root window and widget object
 root = tk.Tk()
 root.title("Button Example")
 
 #Create a button that pops out with the words "Click Me!"
 button = tk.Button(root,text="Click Me!", command=button_click)
 button.pack()
-#
+#execute a loop that lasts forever waiting for events from user. 
 
 root.mainloop()
