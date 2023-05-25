81f4ef3aeaa30cb640f0003e37730b27a3ab9b9d
Justine Cacho <justinecacho@gmail.com>
2023-05-25 10:39:58 -0700

    finish combo box code

--- ComboBox.py
+++ ComboBox.py
@@ -5,10 +5,22 @@ from tkinter import ttk
 #Create an item onject that obtains the value of the selected item.
 def on_select(event):
 
-    selected_item = event.widget{}
-    print(Selected item:", selected_item")
+    selected_item = event.widget.get()
+    print("Selected item", selected_item)
 
 
 #create a root window and widget object
-    root = tk.Tk()
-    root.title("Combobox Example")
+root = tk.Tk()
+root.title("Combobox Example")
+
+#create an array of items
+items = ["Item1", "Item 2", "Item 3", "Item 4", "Item5"]
+
+#Combo box operator and call array of items
+combo_box = ttk.Combobox(root, values=items)
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+combo_box.pack()
+
+#Create a loop to keep window open
+root.mainloop()
