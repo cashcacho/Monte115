9184fdef47c57a8cd0c5ba8005949046aa865480
Justine Cacho <justinecacho@gmail.com>
2023-05-25 21:11:46 -0700

    fix comments

--- Button_clicked.py
+++ Button_clicked.py
@@ -6,13 +6,13 @@ def button_click ():
     print("Button clicked")
 
 
-#
+#create root window and widjet object
 root = tk.Tk()
 root.title("Button Example")
 
 #Create a button that pops out with the words "Click Me!"
 button = tk.Button(root,text="Click Me!", command=button_click)
 button.pack()
-#
 
+#Create loop to keep window open
 root.mainloop()
